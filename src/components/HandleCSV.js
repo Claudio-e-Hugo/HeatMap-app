4992699bfafb8a9e7a4660926c0e25f93ff846fa
Hugo <h.domingos@ua.pt>
2022-10-05 19:53:11 +0100

    first commit

---
+++ src/components/HandleCSV.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Papa from 'papaparse';
+
+class HandleCSV extends React.Component {
+    render() { 
+        return (
+            this.parseCoordinatesFromCsv(this.props)
+        );
+    }
+
+    parseCoordinatesFromCsv(props) {
+        console.log(props.fileName);
+    }
+}
+ 
+export default HandleCSV;
