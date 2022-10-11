a3ce7df216662199411546e2fe4b7e6c2be48714
Hugo <h.domingos@ua.pt>
2022-10-11 14:37:06 +0100

    checkboxes to select post

--- src/App.js
+++ src/App.js
@@ -8,13 +8,13 @@ export default function App() {
     return (
         <div class="container">
             <div class="row">
-                <div class="col-sm">
-                </div>
+                {/* <div class="col-sm-1">
+                </div> */}
                 <div class="col">
                     <Map/>
                 </div>
-                <div class="col-sm">
-                </div>
+                {/* <div class="col-sm-1">
+                </div> */}
             </div>
         </div>
     );
--- src/components/Map.js
+++ src/components/Map.js
@@ -19,6 +19,7 @@ import '../App.css'
 import '../data/lines1.json'
 import {useEffect, useState} from 'react';
 import { Container } from '@mui/system';
+import Checkbox from '@mui/material/Checkbox';
 
 
 const center = [40.6464534, -8.6536617];
@@ -30,13 +31,18 @@ const p19_coords = [40.64339, -8.65847];
 const coords = getLinesFromJson();
 
 const p15_bitrate = getP15FromJson();
+const p19_bitrate = getP19FromJson();
 
 var color="";
 
+const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
+
+const post = "p15";
 
 function Map() {
 
     const [heat, setHeat] = useState("");
+    const [post, setPost] = useState([]);
     
     useEffect(() => {
         console.log('Heat is now: ', heat);
@@ -44,109 +50,205 @@ function Map() {
 
     const handleChange = (event) => {
         setHeat(event.target.value);
-        console.log(heat);
+        // console.log(heat);
     }
+
+    const handleChangeCheckBox = (event) => {
+        const currentIndex = post.indexOf(event);
+        const newPost = [...post];
+        if(currentIndex === -1) {
+            newPost.push(event);
+        } else {
+            newPost.splice(currentIndex, 1);
+        }
+        setPost(newPost);
+        console.log(post);
+    }
+    
     return (
         <div>
-            <MapContainer id="map-container"
-                center={center}
-                zoom={14}
-                minZoom={15}
-                maxZoom={28}
-                style={{width: '80vw', height: '80vh'}}
-                >
-                <TileLayer
-                    url='https://api.maptiler.com/maps/basic-v2/256/{z}/{x}/{y}.png?key=GCrdp2KFceg0vK7Ifepx'
-                />
-
-                {
-                    Object.keys(coords).map((street) => {
-                        return(
-                            <Polyline key={street} color={"black"} opacity={"50%"} positions={coords[street]}></Polyline>
-                        );
-                    })
-                }
-                {
-                    p15_bitrate.map((segment) => {
-                        //if radio button is selected, show the circles
-                        if(heat === "bitrate"){
-                            color = "hsl(" + (segment.bitrate*15) + ", 100%, 50%)";
-                            console.log(segment.bitrate);
-                        }
-                        if(heat === "jitter"){
-                            color = "hsl(" + ((120 - segment.jitter)) + ", 100%, 50%)";  
-                            console.log(segment.jitter);
-                        }
-                        if(heat === "ploss"){ 
-                            color = "hsl(" + ((120-segment.lost )) + ", 100%, 50%)";
-                            console.log(segment.lost);
-                        }
-                        
-                        return(
-                            <CircleMarker center={[segment.lat, segment.long]} radius={2}
-                            pathOptions={{ color: color }}
-                            eventHandlers={{
-                                //check the heat value and show the popup with the right data
-                                
-                                                            
-                                mouseover: (event) => {
-                                    if(heat === "bitrate"){
-                                        event.target.bindPopup("Bitrate: " + segment.bitrate + " kbps").openPopup();
-                                    }
-                                    if(heat === "jitter"){
-                                        event.target.bindPopup("Jitter: " + segment.jitter + " ms").openPopup();
-                                    }
-                                    if(heat === "ploss"){
-                                        event.target.bindPopup("Packet Loss: " + segment.lost + " %").openPopup();
-                                    }
-                                    event.target.openPopup()},
-                                mouseout: (event) => event.target.closePopup(),
-
-
-                            }}
+            <div class="container-fluid">
+            <div class="row">
+                <div class="col-sm-1">
+                    <div class="float-left">
+                        <FormControl>
+                            <FormLabel id="info">Info</FormLabel>
+                            <RadioGroup
+                                aria-labelledby="demo-radio-buttons-group-label"
+                                defaultValue="bitrate"
+                                name="radio-buttons-group"
                             >
+                                <FormControlLabel onChange={handleChange} value="bitrate" control={<Radio />} label="BitRate" />
+                                <FormControlLabel onChange={handleChange} value="jitter" control={<Radio />} label="Jitter" />
+                                <FormControlLabel onChange={handleChange} value="ploss" control={<Radio />} label="Packet Loss" />
+                            </RadioGroup>
+                        </FormControl>
+                        <div>
+                        <FormControlLabel
+                            control={
+                            <Checkbox value="p15" onChange={handleChangeCheckBox}/>
+                            }
+                            label="P15"
+                        />
+                        <FormControlLabel
+                            control={
+                            <Checkbox value="p19" onChange={handleChangeCheckBox}/>
+                            }
+                            label="P19"
+                        />
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-8">
+                    <MapContainer id="map-container"
+                        center={center}
+                        zoom={14}
+                        minZoom={15}
+                        maxZoom={28}
+                        style={{width: '60vw', height: '60vh'}}
+                        >
+                        <TileLayer
+                            url='https://api.maptiler.com/maps/basic-v2/256/{z}/{x}/{y}.png?key=GCrdp2KFceg0vK7Ifepx'
+                        />
+
+                        {
+                            Object.keys(coords).map((street) => {
+                                return(
+                                    <Polyline key={street} color={"black"} opacity={"50%"} positions={coords[street]}></Polyline>
+                                );
+                            })
+                        }
+
+                        {
+                            post == "p15" ? 
+                            p15_bitrate.map((segment) => {
+                                //if radio button is selected, show the circles
+                                if(heat === "bitrate"){
+                                    color = "hsl(" + (segment.bitrate*15) + ", 100%, 50%)";
+                                    // console.log(segment.bitrate);
+                                } else if(heat === "jitter"){
+                                    color = "hsl(" + ((120 - segment.jitter)) + ", 100%, 50%)";  
+                                    // console.log(segment.jitter);
+                                } else if(heat === "ploss"){ 
+                                    color = "hsl(" + ((120-segment.lost )) + ", 100%, 50%)";
+                                    // console.log(segment.lost);
+                                } else {
+                                    color = "hsl(" + (segment.bitrate*15) + ", 100%, 50%)";
+                                    // console.log(segment.bitrate);
+                                }
                                 
-                                {/* if(heat === "bitrate"){
-                                    <Popup>{segment.bitrate}</Popup>
-                                }
-                                if(heat === "jitter"){
-                                    <Popup>{segment.jitter}</Popup>
-                                }
-                                if(heat === "ploss"){
-                                    <Popup>{segment.lost}</Popup>
-                                } */}
+                                return(
+                                    <CircleMarker center={[segment.lat, segment.long]} radius={2}
+                                    pathOptions={{ color: color }}
+                                    eventHandlers={{
+                                        //check the heat value and show the popup with the right data
+                                        
+                                                                    
+                                        mouseover: (event) => {
+                                            if(heat === "bitrate"){
+                                                event.target.bindPopup("Bitrate: " + segment.bitrate + " kbps").openPopup();
+                                            }
+                                            if(heat === "jitter"){
+                                                event.target.bindPopup("Jitter: " + segment.jitter + " ms").openPopup();
+                                            }
+                                            if(heat === "ploss"){
+                                                event.target.bindPopup("Packet Loss: " + segment.lost + " %").openPopup();
+                                            }
+                                            event.target.openPopup()},
+                                        mouseout: (event) => event.target.closePopup(),
 
-                            </CircleMarker>
 
-                        );
+                                    }}
+                                    >
+                                        
+                                        {/* if(heat === "bitrate"){
+                                            <Popup>{segment.bitrate}</Popup>
+                                        }
+                                        if(heat === "jitter"){
+                                            <Popup>{segment.jitter}</Popup>
+                                        }
+                                        if(heat === "ploss"){
+                                            <Popup>{segment.lost}</Popup>
+                                        } */}
+
+                                    </CircleMarker>
+
+                                );
+                                
+                            })
+                            :
+                            p19_bitrate.map((segment) => {
+                                //if radio button is selected, show the circles
+                                if(heat === "bitrate"){
+                                    color = "hsl(" + (segment.bitrate*15) + ", 100%, 50%)";
+                                    // console.log(segment.bitrate);
+                                } else if(heat === "jitter"){
+                                    color = "hsl(" + ((120 - segment.jitter)) + ", 100%, 50%)";  
+                                    // console.log(segment.jitter);
+                                } else if(heat === "ploss"){ 
+                                    color = "hsl(" + ((120-segment.lost )) + ", 100%, 50%)";
+                                    // console.log(segment.lost);
+                                } else {
+                                    color = "hsl(" + (segment.bitrate*15) + ", 100%, 50%)";
+                                    // console.log(segment.bitrate);
+                                }
+                                
+                                return(
+                                    <CircleMarker center={[segment.lat, segment.long]} radius={2}
+                                    pathOptions={{ color: color }}
+                                    eventHandlers={{
+                                        //check the heat value and show the popup with the right data
+                                        
+                                                                    
+                                        mouseover: (event) => {
+                                            if(heat === "bitrate"){
+                                                event.target.bindPopup("Bitrate: " + segment.bitrate + " kbps").openPopup();
+                                            }
+                                            if(heat === "jitter"){
+                                                event.target.bindPopup("Jitter: " + segment.jitter + " ms").openPopup();
+                                            }
+                                            if(heat === "ploss"){
+                                                event.target.bindPopup("Packet Loss: " + segment.lost + " %").openPopup();
+                                            }
+                                            event.target.openPopup()},
+                                        mouseout: (event) => event.target.closePopup(),
+
+
+                                    }}
+                                    >
+                                        
+                                        {/* if(heat === "bitrate"){
+                                            <Popup>{segment.bitrate}</Popup>
+                                        }
+                                        if(heat === "jitter"){
+                                            <Popup>{segment.jitter}</Popup>
+                                        }
+                                        if(heat === "ploss"){
+                                            <Popup>{segment.lost}</Popup>
+                                        } */}
+
+                                    </CircleMarker>
+
+                                );
+                                
+                            })
+                        }
                         
-                    })
-                }
-                
-                <Marker position={p15_coords} icon={new Icon({iconUrl: markerIconPng, iconSize: [25, 41], iconAnchor: [12, 41]})}>
-                    <Popup>
-                        P 15
-                    </Popup>
-                </Marker>
-                <Marker position={p19_coords} icon={new Icon({iconUrl: markerIconPng, iconSize: [25, 41], iconAnchor: [12, 41]})}>
-                    <Popup>
-                        P 19
-                    </Popup>
-                </Marker>
-            </MapContainer>
-            
-            <FormControl>
-                <FormLabel id="info">Info</FormLabel>
-                <RadioGroup
-                    aria-labelledby="demo-radio-buttons-group-label"
-                    defaultValue=""
-                    name="radio-buttons-group"
-                >
-                    <FormControlLabel onChange={handleChange} value="bitrate" control={<Radio />} label="BitRate" />
-                    <FormControlLabel onChange={handleChange} value="jitter" control={<Radio />} label="Jitter" />
-                    <FormControlLabel onChange={handleChange} value="ploss" control={<Radio />} label="Packet Loss" />
-                </RadioGroup>
-            </FormControl>
+                        <Marker position={p15_coords} icon={new Icon({iconUrl: markerIconPng, iconSize: [25, 41], iconAnchor: [12, 41]})}>
+                            <Popup>
+                                P 15
+                            </Popup>
+                        </Marker>
+                        <Marker position={p19_coords} icon={new Icon({iconUrl: markerIconPng, iconSize: [25, 41], iconAnchor: [12, 41]})}>
+                            <Popup>
+                                P 19
+                            </Popup>
+                        </Marker>
+                    </MapContainer>
+                </div>
+
+            </div>
+        </div>
     </div>
     );
 }
@@ -159,6 +261,11 @@ function getLinesFromJson() {
 //loads the p15 data from json file
 function getP15FromJson() {
     return require('../data/data_p15.json');
+}
+
+//loads the p19 data from json file
+function getP19FromJson() {
+    return require('../data/data_p19.json');
 }
 
 export default Map;
